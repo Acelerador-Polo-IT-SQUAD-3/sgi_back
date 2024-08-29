5eb5e1ee128ea232239643531c5516ad5de532a8
IgnacioSenicen <senicenmuoz@gmail.com>
2024-08-29 18:53:57 -0300

    Se creo el crud de members con sus controladores, se añadieron las rutas para estos, durante la carga se especifica la fecha esto se añadio en members, users y autentication

---
+++ src/controllers/members.js
@@ -0,0 +1,88 @@
+import {pool} from '../database.js'
+
+export const createItem = async(req, res) => {
+    // Lógica para crear un nuevo elemento
+
+    try {
+        console.log('Datos recibidos en req.body:', req.body);
+        const { team_id, user_id} = req.body;
+        const [result2] = await pool.query('SELECT id, name, description FROM teams WHERE id = ?', [team_id]);
+        if (result2.length === 0) {
+            return res.status(401).json({ message: 'Equipo no encontrado' });
+        }
+        else{
+            const fecha = new Date();
+            const [result] = await pool.query('INSERT INTO members (team_id, user_id, created_at, updated_at) VALUES(?, ?, ?, ?)', [team_id, user_id, fecha, fecha]);
+            res.json(result[0]);
+        }
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al ingresar al equipo' });
+    }
+};
+
+export const getItem = async (req, res) => {
+    // Lógica para obtener un elemento
+    try {
+        const { id } = req.params;
+        const [result] = await pool.query('SELECT id, team_id, user_id FROM members WHERE id = ?', [id]);
+
+        if (result.length === 0) {
+            return res.status(401).json({ message: 'Usuario no encontrado' });
+        }
+
+        res.json(result[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener el usuario' });
+    }
+};
+
+
+export const getItems = async (req, res) => {
+    // Lógica para obtener todos los elementos
+    try {
+        const [result] = await pool.query('SELECT id, team_id, user_id FROM members');
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener los usuarios' });
+    }
+};
+
+export const updateItem = async(req, res) => {
+    // Lógica para actualizar un elemento por id
+
+    try {
+        console.log('Datos recibidos en req.body:', req.body);
+        const { team_id} = req.body;
+        const { id } = req.params;
+        const fecha = new Date();
+        const [result] = await pool.query('UPDATE members SET team_id=?, updated_at=? WHERE id=?', [team_id, fecha, id]);
+        res.json(result[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al actualizar el usuario' });
+    }
+
+};
+
+export const deleteItem = async (req, res) => {
+    // Lógica para eliminar un elemento por id
+    try {
+        const { id } = req.params;
+        const [result] = await pool.query('DELETE FROM members WHERE id = ?', [id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(401).json({ message: 'Usuario no encontrado' });
+        }
+
+        res.json({ message: 'Usuario eliminado' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener el usuario' });
+    }
+};
+
+export default { createItem, getItems, getItem, updateItem, deleteItem };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import indexRoutes from './routes/index.routes.js';
 import userRoutes from './routes/user.routes.js';
 import autenticationRoutes from './routes/autentication.routes.js';
+import membersRoutes from './routes/members.routes.js';
 
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use('/user', userRoutes);
 app.use('/auth', autenticationRoutes);
+app.use('/memb', membersRoutes);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
