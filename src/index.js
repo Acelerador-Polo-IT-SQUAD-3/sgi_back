053e46af7f278246dbdc2891b71a03e386cb6265
carminepi <carmin.espinoza@epidataconsulting.com>
2024-08-29 18:40:33 -0400

    Delete index.routes.js - test deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import indexRoutes from './routes/index.routes.js';
 import userRoutes from './routes/user.routes.js';
 import autenticationRoutes from './routes/autentication.routes.js';
 
@@ -10,7 +9,6 @@ const PORT = process.env.PORT || 3000;
 // Middleware para parsear JSON debe ir antes de las rutas
 app.use(express.json());
 
-app.use(indexRoutes);
 app.use('/user', userRoutes);
 app.use('/auth', autenticationRoutes);
 
