f380953eebce4a2f5e3ab26c9aa1883cf62b6f3e
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-08-31 20:13:00 -0300

    Agreg componentes para enviar email

--- src/controllers/Email/contend.js
+++ src/controllers/Email/contend.js
@@ -6,7 +6,7 @@ export async function SendingMessagePersonalized (req, res) {
     const { emailSubject, emailBody } = req.body
     const email = "Hola" //Aqui debe ir el user con la sesionn activa
     
-    return sendEmail(res, email, emailSubject, emailBody, 'Email notification for password reset sent')
+    return sendEmail(res, email, emailSubject, emailBody, 'Email notification for poliIt')
 
   } catch (error) {
     return res.status(400).json({ message: error.message, error: "Fallo el envío" });
