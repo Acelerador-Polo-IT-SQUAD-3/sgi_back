50f2945aa23cf1a999be56fffb55335a40d70c46
IgnacioSenicen <senicenmuoz@gmail.com>
2024-09-02 19:48:57 -0300

    Integrar manejo de cors en el backend y Revisión de servicio de login, no se envía el rol_id completo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import userRoutes from './routes/user.routes.js';
 import autenticationRoutes from './routes/autentication.routes.js';
 import membersRoutes from './routes/members.routes.js';
-
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,12 +10,18 @@ const PORT = process.env.PORT || 3000;
 // Middleware para parsear JSON debe ir antes de las rutas
 app.use(express.json());
 
+app.use(cors({
+    origin: 'http://localhost:8100/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+
+
 app.use('/user', userRoutes);
 app.use('/auth', autenticationRoutes);
 app.use('/memb', membersRoutes);
 
 
-
 app.listen(PORT, () => {
-    console.log('Server running on port 3000');
+    console.log('Server running on port '+ PORT);
 });
