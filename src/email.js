0e20645cbf63f4f22756739aa4722635c4a5fb18
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-09-14 18:23:40 -0300

    Add dataSquema

--- src/email.js
+++ src/email.js
@@ -1,12 +1,12 @@
 import {nodemailer} from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-  host: "mail.gmx.com",
-  port: 587,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
 });
 
