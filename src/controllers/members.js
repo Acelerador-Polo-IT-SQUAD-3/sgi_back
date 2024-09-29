1c546788197867feec92d04c9f88c39bed12e3d8
IgnacioSenicen <senicenmuoz@gmail.com>
2024-09-29 19:41:11 -0300

    se crea el servicio de asignacion de equipos

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -23,6 +23,25 @@ export const createItem = async(req, res) => {
     }
 };
 
+export const createMentor = async (team_id, user_id) => {
+    try {
+        const [result2] = await pool.query('SELECT id, name, description FROM teams WHERE id = ?', [team_id]);
+        if (result2.length === 0) {
+            return { status: 401, message: 'Equipo no encontrado' }; // Devuelve un estado y un mensaje
+        } else {
+            const fecha = new Date();
+            const [result] = await pool.query('INSERT INTO members (team_id, user_id, created_at, updated_at) VALUES(?, ?, ?, ?)', [team_id, user_id, fecha, fecha]);
+            console.log("ESTE ES EL MENTOR DESDE LA FUNCION" + result.insertId)
+            console.log("ESTE ES EL MENTOR DESDE LA FUNCION" + result[0])
+            return { status: 200, resultM: result.insertId }; // Devuelve un estado y el resultado
+        }
+    } catch (error) {
+        console.error(error);
+        return { status: 500, message: 'Error al ingresar al equipo' }; // Devuelve un estado y un mensaje de error
+    }
+};
+
+
 export const getItem = async (req, res) => {
     // Lógica para obtener un elemento
     try {
@@ -86,4 +105,27 @@ export const deleteItem = async (req, res) => {
     }
 };
 
-export default { createItem, getItems, getItem, updateItem, deleteItem };
+
+export const massCreateItem = async (userId, teamId) => {
+    try {
+        // Verificar si el equipo existe
+        const [result2] = await pool.query('SELECT id, name, description FROM teams WHERE id = ?', [teamId]);
+        if (result2.length === 0) {
+            throw new Error('Equipo no encontrado'); // Lanzar un error si el equipo no se encuentra
+        }
+        
+        // Insertar el nuevo miembro
+        const fecha = new Date();
+        const [result] = await pool.query('INSERT INTO members (team_id, user_id, created_at, updated_at) VALUES (?, ?, ?, ?)', [teamId, userId, fecha, fecha]);
+        
+        console.log('Nuevo miembro creado:', result);
+        return result; // Retornar el resultado de la inserción
+
+    } catch (error) {
+        console.error('Error al crear el miembro:', error);
+        throw error; // Lanzar el error para que sea manejado por el controlador
+    }
+};
+
+
+export default { createItem, getItems, getItem, updateItem, deleteItem, massCreateItem, createMentor };
