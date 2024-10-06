48fd10046450fb8727254d45205db3439a787e4b
Ludmila Muñoz <51221669+LudmiM@users.noreply.github.com>
2024-10-06 00:30:56 -0300

    Update autentication.js

--- src/validators/autentication.js
+++ src/validators/autentication.js
@@ -8,6 +8,30 @@ export const validateCreate = [
         .not()
         .isEmpty(),
 
+    check('surname')
+        .exists()
+        .custom(verificarLargo)
+        .not()
+        .isEmpty(),
+
+    check('dni')
+        .exists()
+        .isNumeric()
+        .custom((value, {req})=>{
+            if(value.length>16 ){
+                throw new Error('error al ingresar el dni: sobrepasa el limite de caracteres')
+            }
+            else{return true}
+        })
+        .not()
+        .isEmpty(),
+
+    check('description')
+        .exists()
+        .custom(verificarLargo)
+        .not()
+        .isEmpty(),
+
     check('email')
         .exists()
         .custom((value, {req})=>{
