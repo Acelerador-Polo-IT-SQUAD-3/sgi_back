b2749da8ae1bd8790b152962da8ce89b0bc4aac4
LudmiM <ludmi.m.1109@gmail.com>
2024-10-18 22:43:00 -0300

    Add validacion

--- src/validators/autentication.js
+++ src/validators/autentication.js
@@ -14,24 +14,6 @@ export const validateCreate = [
         .not()
         .isEmpty(),
 
-    check('dni')
-        .exists()
-        .isNumeric()
-        .custom((value, {req})=>{
-            if(value.length>16 ){
-                throw new Error('error al ingresar el dni: sobrepasa el limite de caracteres')
-            }
-            else{return true}
-        })
-        .not()
-        .isEmpty(),
-
-    check('description')
-        .exists()
-        .custom(verificarLargo)
-        .not()
-        .isEmpty(),
-
     check('email')
         .exists()
         .custom((value, {req})=>{
@@ -49,18 +31,6 @@ export const validateCreate = [
         .custom(verificarLargo)
         .not()
         .isEmpty(),
-
-    /*check('role_id')
-        .exists()
-        .isNumeric()
-        .custom((value, {req})=>{
-            if(value <1 || value>3 ){
-                throw new Error('error al ingresar el rol')
-            }
-            else{return true}
-        })
-        .not()
-        .isEmpty(),*/
 
     (req, res, next) =>{
         validateResult(req, res, next)        
